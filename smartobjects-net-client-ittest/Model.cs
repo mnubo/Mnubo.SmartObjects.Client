8340667d1cb9c89c37cae59dba25ec223279b391
David Francoeur <dfrancoeur04@gmail.com>
2018-01-19 10:23:07 -0500

    fix(tests): ensure model it test check for greater than count

--- smartobjects-net-client-ittest/Model.cs
+++ smartobjects-net-client-ittest/Model.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using Mnubo.SmartObjects.Client.Impl;
 using Mnubo.SmartObjects.Client.Config;
 using Mnubo.SmartObjects.Client.Models.DataModel;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 
@@ -22,12 +23,12 @@ namespace Mnubo.SmartObjects.Client.ITTest
         public void TestExport()
         {
             Model model = client.Model.Export();
-            Assert.AreEqual(2, model.EventTypes.Count, "event types count");
-            Assert.AreEqual(2, model.Timeseries.Count, "timeseries count");
-            Assert.AreEqual(1, model.ObjectTypes.Count, "object types count");
-            Assert.AreEqual(1, model.ObjectAttributes.Count, "object attributes count");
-            Assert.AreEqual(1, model.OwnerAttributes.Count, "owner attributes count");
-            Assert.AreEqual(1, model.Sessionizers.Count, "sessionizers count");
+            Assert.GreaterOrEqual(model.EventTypes.Count, 2, "event types count");
+            Assert.GreaterOrEqual(model.Timeseries.Count, 2, "timeseries count");
+            Assert.GreaterOrEqual(model.ObjectTypes.Count, 1, "object types count");
+            Assert.GreaterOrEqual(model.ObjectAttributes.Count, 1, "object attributes count");
+            Assert.GreaterOrEqual(model.OwnerAttributes.Count, 1, "owner attributes count");
+            Assert.GreaterOrEqual(model.Sessionizers.Count, 1, "sessionizers count");
 
         }
     }
