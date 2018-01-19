fc95d241e1c978776946b584eec389258f7100b6
David Francoeur <dfrancoeur04@gmail.com>
2018-01-19 10:00:09 -0500

    test(modeler): add it test for new modeler API

--- smartobjects-net-client-ittest/Model.cs
+++ smartobjects-net-client-ittest/Model.cs
@@ -31,5 +31,62 @@ namespace Mnubo.SmartObjects.Client.ITTest
             Assert.GreaterOrEqual(model.Sessionizers.Count, 1, "sessionizers count");
 
         }
+
+        [Test()]
+        public void TestGets()
+        {
+            Assert.GreaterOrEqual(client.Model.GetEventTypes().Count, 2, "event types count");
+            Assert.GreaterOrEqual(client.Model.GetObjectTypes().Count, 1, "object types count");
+            Assert.GreaterOrEqual(client.Model.GetObjectAttributes().Count, 1, "object attributes count");
+            Assert.GreaterOrEqual(client.Model.GetTimeseries().Count, 2, "timeseries count");
+            Assert.GreaterOrEqual(client.Model.GetOwnerAttributes().Count, 1, "owner attributes count");
+
+        }
+
+        [Test()]
+        public void TestSandboxOps()
+        {
+            //we can't reset in it test, all sdk integration tests use the same namespace
+            //client.Model.SandboxOps.ResetOps.Reset();
+
+            string key = Guid.NewGuid().ToString().Replace("-", "");
+            client.Model.SandboxOps.EventTypesOps.CreateOne(new EventType(
+                key, "", "scheduled", new List<string>()
+            ));
+            client.Model.SandboxOps.EventTypesOps.Update(key, new EventType(
+                key, "new desc", "unscheduled", new List<string>()
+            ));
+
+            client.Model.SandboxOps.ObjectTypesOps.CreateOne(new ObjectType(
+                key, "", new List<string>()
+            ));
+            client.Model.SandboxOps.ObjectTypesOps.Update(key, new ObjectType(
+                key, "new desc", new List<string>()
+            ));
+
+            string tsKey = key + "-ts";
+            client.Model.SandboxOps.TimeseriesOps.CreateOne(new Timeseries(
+                tsKey, "", "", "TEXT", new List<string>() { key }
+            ));
+            client.Model.SandboxOps.TimeseriesOps.Update(tsKey, new UpdateEntity("new display name", "new desc"));
+            client.Model.SandboxOps.TimeseriesOps.Deploy(tsKey);
+
+            string objKey = key + "-obj";
+            client.Model.SandboxOps.ObjectAttributesOps.CreateOne(new ObjectAttribute(
+                objKey, "", "", "FLOAT", "none", new List<string>() { key }
+            ));
+            client.Model.SandboxOps.ObjectAttributesOps.Update(objKey, new UpdateEntity("new display name", "new desc"));
+            client.Model.SandboxOps.ObjectAttributesOps.Deploy(objKey);
+
+            string ownerKey = key + "-own";
+            client.Model.SandboxOps.OwnerAttributesOps.CreateOne(new OwnerAttribute(
+                ownerKey, "", "", "DOUBLE", "none"
+            ));
+            client.Model.SandboxOps.OwnerAttributesOps.Update(ownerKey, new UpdateEntity("new display name", "new desc"));
+            client.Model.SandboxOps.OwnerAttributesOps.Deploy(ownerKey);
+
+            client.Model.SandboxOps.EventTypesOps.Delete(key);
+            client.Model.SandboxOps.ObjectTypesOps.Delete(key);
+        }
     }
 }
