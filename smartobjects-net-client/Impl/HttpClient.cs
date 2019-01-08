mnubo/Mnubo.SmartObjects.Client

commit b780d738d37a6f9f813647245937f9efa45ce343

fix(version): the sdk is broken because the version string a new line

smartobjects-net-client/Impl/HttpClient.cs
@@ -42,7 +42,7 @@ namespace Mnubo.SmartObjects.Client.Impl
                     var _assembly = Assembly.GetExecutingAssembly();
                     var _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("Mnubo.SmartObjects.Client.Version.txt"));
                     var content = _textStreamReader.ReadToEnd();
-                    return content.Split('=')[1];
+                    return content.Split('=')[1].Replace(System.Environment.NewLine, "");
                 }
                 catch
                 {

commit 186a60e2d4eef912ee9d2a6fab190b53f38d2805

fix(http): the request is not sent because the request is malformed
On Windows, the new line was not removed properly from the SDK version
header. The commit b780d738d37a6f9f813647245937f9efa45ce343 introduced
a fix that I believed work on all OSes but I was wrong, it did not work
on Windows.

smartobjects-net-client/Impl/HttpClient.cs
@@ -42,7 +42,7 @@ namespace Mnubo.SmartObjects.Client.Impl
                     var _assembly = Assembly.GetExecutingAssembly();
                     var _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("Mnubo.SmartObjects.Client.Version.txt"));
                     var content = _textStreamReader.ReadToEnd();
-                    return content.Split('=')[1].Replace(System.Environment.NewLine, "");
+                    return content.Split('=')[1].Replace(System.Environment.NewLine, "").TrimEnd(Environment.NewLine.ToCharArray());
                 }
                 catch
                 {