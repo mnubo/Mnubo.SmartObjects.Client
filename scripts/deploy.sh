3d25a689908cdd36c44e86dbd61255a31d838b51
Jonathan Hallee <jonathan.hallee@aspentech.com>
2021-02-17 17:25:33 -0500

    chore(publishing): change publishing to .sh file to respect Travis requirement

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dotnet pack ../AspenTech.SmartObjects.Client/AspenTech.SmartObjects.Client.csproj -p:NuspecFile=../AspenTech.SmartObjects.Client.nuspec -c Release
+dotnet nuget push AspenTech.SmartObjects.Client/bin/Release/*.nupkg --api-key $1 --source https://api.nuget.org/v3/index.json --skip-duplicate
