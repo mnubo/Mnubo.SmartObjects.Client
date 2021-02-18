2d466c6c42f12b7546ed68647593b1178056def5
Jonathan Hallee <jonathan.hallee@aspentech.com>
2021-02-18 11:37:24 -0500

    chore(publishing): fix paths to dotnet pack

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ux
 
-dotnet pack ../AspenTech.SmartObjects.Client/AspenTech.SmartObjects.Client.csproj -p:NuspecFile=../AspenTech.SmartObjects.Client.nuspec -c Release
-dotnet nuget push ../AspenTech.SmartObjects.Client/bin/Release/*.nupkg --api-key $1 --source https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet pack AspenTech.SmartObjects.Client/AspenTech.SmartObjects.Client.csproj -p:NuspecFile=../AspenTech.SmartObjects.Client.nuspec -c Release
+dotnet nuget push AspenTech.SmartObjects.Client/bin/Release/*.nupkg --api-key $1 --source https://api.nuget.org/v3/index.json --skip-duplicate
